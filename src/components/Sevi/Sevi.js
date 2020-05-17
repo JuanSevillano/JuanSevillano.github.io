61e320266e8ac14e26beaa3e836c4a9e375861c1
JuanSevillano <sevi0_6@hotmail.com>
2020-05-17 16:49:30 +0200

    logo created

---
+++ src/components/Sevi/Sevi.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Sevi.module.css'
+
+const Sevi = (props) => {
+	return (
+		<div className={classes.Sevi}>
+			<p id={classes.SEV}>sev</p><p id={classes.I}>i</p>
+		</div>
+	)
+}
+
+export default Sevi
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Sevi from '../../components/Sevi/Sevi'
+import classes from './Home.module.css'
+
+const Home = (props) => {
+	return (
+		<div className={classes.Home}>
+			<Sevi />
+		</div>
+	)
+}
+
+export default Home
